1be250ba0659bcfff3312023ed4eea2f640c977d
Sebastien <sebastienlegerperre@gmail.com>
2024-12-28 01:39:46 -0800

    completed day 6 part 2

--- day6/part2.py
+++ day6/part2.py
@@ -1,35 +1,60 @@
+import copy
 f = open("input.txt")
 
-rules = []
-orderings = []
+matrix = []
 for newline in f:
-    if "|" in newline:
-        rules.append(newline.strip())
-    else:
-        break
+    matrix.append(list(newline.strip()))
 
-for newline in f:
-    orderings.append(newline.strip())
+start_pos = None
 
-ans = 0
+for i, row in enumerate(matrix):
+    for j, col in enumerate(row):
+        if matrix[i][j] == "^":
+            start_pos = (i, j)
 
-for ord in orderings:
-    ordering = ord.split(",")
-    val = True
-    for i in range(len(ordering) - 1):
-        not_valid = ordering[i + 1] + "|" + ordering[i]
-        if not_valid in rules:
-            val = False
+dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+dir_index = 0
+
+positions = []
+
+matrix[start_pos[0]][start_pos[1]] = "*"
+curr_pos = start_pos
+
+c = copy.deepcopy(matrix)
+
+while 0 <= curr_pos[0] < len(matrix) and 0 <= curr_pos[1] < len(matrix[0]):
+    if matrix[curr_pos[0]][curr_pos[1]] == ".":
+        positions.append(curr_pos)
+        matrix[curr_pos[0]][curr_pos[1]] = "*"
+    elif matrix[curr_pos[0]][curr_pos[1]] == "#":
+        curr_pos = (curr_pos[0] - dirs[dir_index][0], curr_pos[1] - dirs[dir_index][1])
+        dir_index += 1
+        dir_index = dir_index % len(dirs)   
+    curr_pos = (curr_pos[0] + dirs[dir_index][0], curr_pos[1] + dirs[dir_index][1])
+
+res = 0
+
+count = 0
+
+for pos_x, pos_y in positions:
+    print(f"On {count} out of {len(positions)}")
+    dir_index = 0
+    matrix = copy.deepcopy(c)
+    matrix[pos_x][pos_y] = "#"
+    curr_pos = start_pos
+    moves = 0
+    while 0 <= curr_pos[0] < len(matrix) and 0 <= curr_pos[1] < len(matrix[0]):
+        if matrix[curr_pos[0]][curr_pos[1]] == ".":
+            matrix[curr_pos[0]][curr_pos[1]] = dir_index
+        elif matrix[curr_pos[0]][curr_pos[1]] == "#":
+            curr_pos = (curr_pos[0] - dirs[dir_index][0], curr_pos[1] - dirs[dir_index][1])
+            dir_index += 1
+            dir_index = dir_index % len(dirs)
+        elif matrix[curr_pos[0]][curr_pos[1]] == dir_index:
+            res += 1
             break
-    if not val:
-        for i in range(len(ordering)):
-            for j in range(i + 1, len(ordering)):
-                not_valid = ordering[j] + "|" + ordering[i]
-                if not_valid in rules:
-                    temp = ordering[j]
-                    ordering[j] = ordering[i]
-                    ordering[i] = temp
-        ans += int(ordering[len(ordering) // 2])
+        curr_pos = (curr_pos[0] + dirs[dir_index][0], curr_pos[1] + dirs[dir_index][1])
+    matrix[pos_x][pos_y] = "."
+    count += 1
 
-print(ans)
-
+print(res)
