8dbdb3543e7771fb7c113db382d3522cd714a60a
Sebastien <sebastienlegerperre@gmail.com>
2025-01-04 20:23:10 -0800

    completed day19 part 2

--- day19/part2.py
+++ day19/part2.py
@@ -11,14 +11,14 @@ memo = {}
 
 def dfs(i, s):
     if i == len(s):
-        return True
+        return 1
     if i in memo:
         return memo[i]
-    valid = False
+    amount = 0
     for towel in towels:
         if len(s) >= i + len(towel) and s[i:i + len(towel)] == towel:
-            valid = valid or dfs(i + len(towel), s)
-    memo[i] = valid
+            amount += dfs(i + len(towel), s)
+    memo[i] = amount
     return memo[i]
 
 res = 0
@@ -26,7 +26,6 @@ res = 0
 for i, pattern in enumerate(patterns):
     print(f"{i} out of {len(patterns)}")
     memo = {}
-    if dfs(0, pattern):
-        res += 1
+    res += dfs(0, pattern)
 
 print(res)
