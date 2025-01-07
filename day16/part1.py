f162099d7685bb2d6fe25906dd75e074b54a9676
Sebastien <sebastienlegerperre@gmail.com>
2025-01-06 18:03:57 -0800

    completed day 16 part 1

--- day16/part1.py
+++ day16/part1.py
@@ -1,4 +1,6 @@
-f = open("testinput.txt")
+import heapq
+
+f = open("input.txt")
 
 grid = []
 for line in f:
@@ -14,8 +16,37 @@ for i, line in enumerate(grid):
         elif cell == "E":
             end = (i, j)
 
-memo = {}
+grid[start[0]][start[1]] = "."
+grid[end[0]][end[1]] = "."
 
-def dfs(i, j, dir):
-    if grid[i + 1][j]
-    dfs()
+queue = []
+
+heapq.heappush(queue, (0, start, 0))
+
+res = None
+
+# Going to the right is clockwise. Going to the left is counterclockwise
+directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+i = 0
+
+visited = set()
+
+while queue:
+    cost, location, direction = heapq.heappop(queue)
+    if location == end:
+        res = cost
+        break
+    
+    if (location, (direction + 1) % 4) not in visited:
+        visited.add((location, (direction + 1) % 4))
+        heapq.heappush(queue, (1000 + cost, location, (direction + 1) % 4))
+    if (location, (direction - 1) % 4) not in visited:
+        visited.add((location, (direction - 1) % 4))
+        heapq.heappush(queue, (1000 + cost, location, (direction - 1) % 4))
+
+    if grid[location[0] + directions[direction][0]][location[1] + directions[direction][1]] != "#" and ((location[0] + directions[direction][0], location[1] + directions[direction][1]), direction) not in visited:
+        visited.add(((location[0] + directions[direction][0], location[1] + directions[direction][1]), direction))
+        heapq.heappush(queue, (1 + cost, (location[0] + directions[direction][0], location[1] + directions[direction][1]), direction))
+
+print(res)
